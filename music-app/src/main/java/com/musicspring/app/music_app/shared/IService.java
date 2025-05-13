8a5d520c3fbe05ab23c2085aca9cad151b0d55d9
Pablo <pablosalompita@gmail.com>
2025-05-13 17:30:18 -0300

    Revert "Fix findAll parameter"
    This reverts commit 4f46b2f42a1eb1bb2663446291baca320967b45b.

--- music-app/src/main/java/com/musicspring/app/music_app/shared/IService.java
+++ music-app/src/main/java/com/musicspring/app/music_app/shared/IService.java
@@ -1,13 +1,12 @@
 package com.musicspring.app.music_app.shared;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface IService<T> {
-    Page<T> findAll(Pageable pageable);
-    T findById(Long id);
-    void deleteById(Long id);
-    void save (T t);
+    public Page<T> findAll();
+    public T findById(Long id);
+    public void deleteById(Long id);
+    public void save (T t);
 }
