747b3ab5e15a2e8665d70d838eb7df56305d5607
Pablo <pablosalompita@gmail.com>
2025-05-13 17:32:12 -0300

    Update IService findById parameter

--- music-app/src/main/java/com/musicspring/app/music_app/shared/IService.java
+++ music-app/src/main/java/com/musicspring/app/music_app/shared/IService.java
@@ -1,12 +1,13 @@
 package com.musicspring.app.music_app.shared;
 
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.Optional;
 
 public interface IService<T> {
-    public Page<T> findAll();
-    public T findById(Long id);
-    public void deleteById(Long id);
-    public void save (T t);
+    Page<T> findAll(Pageable pageable);
+    T findById(Long id);
+    void deleteById(Long id);
+    void save (T t);
 }
