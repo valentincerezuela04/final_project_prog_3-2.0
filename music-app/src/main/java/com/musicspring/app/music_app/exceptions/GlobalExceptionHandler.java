09722423faebd7829bde681dabb95488b45ba890
Pablo <pablosalompita@gmail.com>
2025-05-14 10:30:50 -0300

    Add ErrorDetails and GlobalExceptionHandler

---
+++ music-app/src/main/java/com/musicspring/app/music_app/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.musicspring.app.music_app.exceptions;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
