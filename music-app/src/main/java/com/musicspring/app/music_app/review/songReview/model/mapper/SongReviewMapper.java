6f1e46898314f2d8ca1964b730dc16d2e4368b26
Manuel Palacios Inza <manuelpalaciosinzacontacto@gmail.com>
2025-05-14 10:24:19 -0300

    add SongReview repository,service,mapper,controller and request

--- music-app/src/main/java/com/musicspring/app/music_app/review/songReview/model/mapper/SongReviewMapper.java
+++ music-app/src/main/java/com/musicspring/app/music_app/review/songReview/model/mapper/SongReviewMapper.java
@@ -1,13 +1,35 @@
 package com.musicspring.app.music_app.review.songReview.model.mapper;
 
+import com.musicspring.app.music_app.review.songReview.model.dto.SongReviewRequest;
 import com.musicspring.app.music_app.review.songReview.model.dto.SongReviewResponse;
 import com.musicspring.app.music_app.review.songReview.model.entity.SongReviewEntity;
+import com.musicspring.app.music_app.song.model.dto.SongResponse;
+import com.musicspring.app.music_app.song.model.entity.SongEntity;
+import com.musicspring.app.music_app.song.repository.SongRepository;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class SongReviewMapper {
 
-    public static SongReviewResponse toResponse(SongReviewEntity songReview) {
+    private final SongRepository songRepository;
+    private final UserRepository userRepository;
+
+    @Autowired
+    public SongReviewMapper(SongRepository songRepository,UserRepository userRepository) {
+        this.songRepository = songRepository;
+        this.userRepository = userRepository;
+    }
+
+    public SongReviewResponse toResponse(SongReviewEntity songReview) {
+        if (songReview == null){
+            return null;
+        }
         return SongReviewResponse.builder()
                 .username(songReview.getUser().getUsername())
                 .rating(songReview.getRating())
@@ -15,5 +37,24 @@ public class SongReviewMapper {
                 .build();
     }
 
-    public static SongReviewEntity toModel()
+    public List<SongReviewResponse> toResponseList(List<SongReviewEntity> songReviews) {
+        if (songReviews == null) {
+            return null;
+        }
+        return songReviews.stream()
+                .map(this::toResponse)
+                .collect(Collectors.toList());
+    }
+    public SongReviewEntity toEntity (SongReviewRequest songReviewRequest){
+        return SongReviewEntity.builder()
+                .active(true)
+                .description(songReviewRequest.getDescription())
+                .rating(songReviewRequest.getRating())
+                .song(songRepository.findById(songReviewRequest.getSongId())
+                        .orElseThrow(()-> new EntityNotFoundException("Song not found")))
+                .user(userRepository.findById(songReviewRequest.getUserId())
+                        .orElseThrow(()->new EntityNotFoundException("User not found")))
+                .build();
+    }
+
 }
--- music-app/src/main/java/com/musicspring/app/music_app/review/songReview/service/SongReviewService.java
+++ music-app/src/main/java/com/musicspring/app/music_app/review/songReview/service/SongReviewService.java
@@ -1,4 +1,48 @@
 package com.musicspring.app.music_app.review.songReview.service;
 
-public class SongReviewService {
+import com.musicspring.app.music_app.review.songReview.model.entity.SongReviewEntity;
+import com.musicspring.app.music_app.review.songReview.repository.SongReviewRepository;
+import com.musicspring.app.music_app.shared.IService;
+import com.musicspring.app.music_app.song.model.entity.SongEntity;
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+@Service
+public class SongReviewService implements IService<SongReviewEntity> {
+
+    private final SongReviewRepository songReviewRepository;
+    public SongReviewService (SongReviewRepository songReviewRepository){
+        this.songReviewRepository = songReviewRepository;
+    }
+
+    @Override
+    public Page<SongReviewEntity> findAll(Pageable pageable) {
+        return songReviewRepository.findAll(pageable);
+    }
+
+    @Override
+    public SongReviewEntity findById(Long id) {
+        return songReviewRepository.findById(id)
+                .orElseThrow(() -> new EntityNotFoundException("Song Review with ID: " + id + " was not found.")); ///Reemplazar por una excepcion personalizada
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        SongReviewEntity songReviewEntity = findById(id);
+        songReviewEntity.setActive(false);
+        songReviewRepository.save(songReviewEntity);
+    }
+
+    @Override
+    public SongReviewEntity save(SongReviewEntity songReview) {
+        return songReviewRepository.save(songReview);
+    }
+
+    public Page<SongReviewEntity> findBySongId (Long songId, Pageable pageable){
+        return songReviewRepository.findBySong_Id(songId,pageable);
+    }
 }
