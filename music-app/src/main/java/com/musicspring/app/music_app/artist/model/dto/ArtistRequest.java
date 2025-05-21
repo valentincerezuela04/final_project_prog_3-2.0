1a841dbf1958e380fce881f7c51ad418ff3f2ccf
Valentin Cerezuela <123900495+valentincerezuela04@users.noreply.github.com>
2025-05-21 09:46:14 -0300

    added swagger documentation

--- music-app/src/main/java/com/musicspring/app/music_app/artist/model/dto/ArtistRequest.java
+++ music-app/src/main/java/com/musicspring/app/music_app/artist/model/dto/ArtistRequest.java
@@ -1,5 +1,6 @@
 package com.musicspring.app.music_app.artist.model.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -11,13 +12,18 @@ import lombok.*;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Schema(name = "ArtistRequest", description = "Request DTO to create or update an artist")
 public class ArtistRequest {
+
     @NotBlank
+    @Schema(description = "Name of the artist", example = "The Beatles", required = true)
     private String name;
 
     @NotNull
     @Min(value = 0, message = "Followers must be non-negative")
+    @Schema(description = "Number of followers the artist has (non-negative integer)", example = "5000000", required = true)
     private Integer followers;
 
+    @Schema(description = "Flag indicating if the artist is currently active", example = "true")
     private boolean active;
 }
